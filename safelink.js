541ed26685e36921b20ecc1d1ae7a004846485b8
mars0512v2 <57721416+mars0512v2@users.noreply.github.com>
2019-11-13 23:23:07 +0700

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -22,7 +22,7 @@ if(a_to_vh==false)
 if(x=="2"){xxx="car-insurance-find-right-coverage.html"}
 if(x=="3"){xxx="apply-for-business-credit-card-to-gain.html"}
 if(x=="4"){xxx="how-to-convert-bitcoins-to-dollars.html"}
-if(x=="5"){xxx="business-news-from-newspaper-to-internet.html"}a_to_ve[i].href="https://wvw.miting.biz/2019/09/"+xxx+"?url="+encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="business-news-from-newspaper-to-internet.html"}a_to_ve[i].href="https://www.miting.biz/2019/09/"+xxx+"?url="+encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
